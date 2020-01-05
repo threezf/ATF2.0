01e3ce69b005b40e3dfe352c5b8bbc77b8db198a
Baojiakangdi <1043010974@qq.com>
2020-01-05 10:17:07 +0800

    修改用例管理，添加用例导入记录

--- src/views/testProject/testCaseManagement.vue
+++ src/views/testProject/testCaseManagement.vue
@@ -25,14 +25,14 @@
 						导入
 					</el-button>
 					<el-button
-						@click=''
+						@click='toUploadRecord'
 						type="primary"
 						size="small"
 						icon="el-icon-upload2">
 						导入记录
 					</el-button>
 					<el-button
-						@click=''
+						@click='outputCase'
 						type="primary"
 						size="small"
 						icon="el-icon-download">
@@ -42,7 +42,8 @@
 						@click=''
 						type="primary"
 						size="small"
-						icon="el-icon-plus">
+						icon="el-icon-plus"
+					@click="addNodeShow">
 						添加流程节点
 					</el-button>
 					<el-button
@@ -66,18 +67,108 @@
 							stripe
 							:data="testCaseList"
 							border
-							style="overflow: hidden"
+							:row-class-name="tableRowClassName"
 							class='table'
+							@expand-change="subShow"
 							@selection-change="handleSelectionChange">
-							<el-table-column type="selection" ></el-table-column>
-							<el-table-column
-								label="节点"
-								property=""
-								width="60">
+							<el-table-column type="expand"  v-if="">
 								<template slot-scope="scope">
-									<i class="el-icon-arrow-down" v-if="scope.row.caseCompositeType!='1'" @click="subShow(scope.row.id)"></i>
+									<div class="scrollbar" v-if="scope.row.caseCompositeType=='2'">
+										<el-scrollbar style="width:100%">
+											<el-table :data="subCaseList" @selection-change="subHandleSelectionChange">
+												<el-table-column
+													type="selection"/>
+												<el-table-column
+													label="被测系统"
+													property="autName"/>
+												<el-table-column
+													label="功能点"
+													property='transName'/>
+												<el-table-column
+													label="用例组成类型">
+													<template slot-scope="scope">
+														{{ '流程节点' }}
+													</template>
+												</el-table-column>
+												<el-table-column
+													label="状态">
+													<template slot-scope="scope">
+														{{ scope.row.useStatus=='0'?'新增':'评审通过' }}
+													</template>
+												</el-table-column>
+												<el-table-column
+													label="关联脚本名称"
+													property='scriptTemplateName'/>
+												<el-table-column
+													label="作者"
+													property='authorName'/>
+												<el-table-column
+													label="执行者"
+													property='executorName'
+													width="120"/>
+												<el-table-column
+													label="评审者"
+													property='reviewerName'/>
+												<el-table-column
+													label="执行方式">
+													<template slot-scope="scope">
+														{{ convertExecMe(scope.row.executeMethod) }}
+													</template>
+												</el-table-column>
+												<el-table-column
+													label="测试任务"
+													property='missionName'/>
+												<el-table-column
+													label="优先级">
+													<template slot-scope="scope">
+														{{ convertPri(scope.row.priority) }}
+													</template>
+												</el-table-column>
+												<el-table-column
+													label="用例类型">
+													<template slot-scope="scope">
+														{{ convertCaseType(scope.row.caseType) }}
+													</template>
+												</el-table-column>
+												<el-table-column
+													label="用例性质">
+													<template slot-scope="scope">
+														{{ convertCasePro(scope.row.caseProperty) }}
+													</template>
+												</el-table-column>
+												<el-table-column label="操作" width="300">
+													<template slot-scope="scope">
+														<el-button
+															size="mini"
+															@click="">查看
+														</el-button>
+														<el-button
+															size="mini"
+															type="info"
+															@click="">修改
+														</el-button>
+														<el-button
+															size="mini"
+															type="danger"
+															@click="">删除
+														</el-button>
+													</template>
+												</el-table-column>
+											</el-table>
+										</el-scrollbar>
+									</div>
 								</template>
 							</el-table-column>
+							<el-table-column type="selection"></el-table-column>
+							<!--							<el-table-column-->
+							<!--								label="节点"-->
+							<!--								property=""-->
+							<!--								width="60">-->
+							<!--								<template slot-scope="scope">-->
+							<!--									<i class="el-icon-arrow-down" v-if="scope.row.caseCompositeType!='1'"-->
+							<!--									   @click="subShow(scope.row.id,scope.row.caseCompositeType,scope.row.index)"></i>-->
+							<!--								</template>-->
+							<!--							</el-table-column>-->
 							<el-table-column
 								label="用例编号"
 								property="casecode"/>
@@ -718,23 +809,198 @@
 					</el-tabs>
 				</el-dialog>
 				<el-dialog
+					title="添加流程节点"
+					:visible.sync="dialogVisibleN"
+					width="65%">
+					<el-tabs v-model="activeName" type="card">
+							<div>
+								<el-form ref="addMultipleForm"  :model="addMultipleForm">
+									<el-row>
+										<el-col :span="18" :offset="4">
+											<el-form-item label="流程节点编号" prop="casecode" label-width="35%" required>
+												<el-col :span="8">
+													<el-input v-model="addMultipleForm.casecode"></el-input>
+												</el-col>
+												<el-col :span="8" :offset='1'>
+													<el-button
+														@click='showVisibleM'
+														type="primary"
+														size="small"
+														icon="el-icon-arrow-down">
+														显示详情
+													</el-button>
+												</el-col>
+											</el-form-item>
+										</el-col>
+									</el-row>
+									<div id="addVisibleM" class="notShowAddSingle">
+										<el-divider content-position="center">后缀有*为必填项目</el-divider>
+										<el-row>
+											<el-col :span="7">
+												<el-form-item label="被测系统" prop="autName" label-width="40%" required>
+													<el-select class="selectWidth" v-model="autValue" size="small"
+															   @change="transChange($event)">
+														<el-option
+															v-for="item in autList"
+															:key="item.id"
+															:label="item.nameMedium"
+															:value="item.id">
+														</el-option>
+													</el-select>
+												</el-form-item>
+											</el-col>
+											<el-col :span="7" :offset="1">
+												<el-form-item label="功能点" prop="transName" label-width="40%"
+															  required>
+													<el-select class="selectWidth" v-model="transValue" size="small"
+															   @change="templateChange($event)">
+														<el-option
+															v-for="item in transList"
+															:key="item.id"
+															:label="item.nameMedium"
+															:value="item.id">
+														</el-option>
+													</el-select>
+												</el-form-item>
+											</el-col>
+											<el-col :span="7" :offset="1">
+												<el-form-item label="基础脚本" prop="scriptTemplateName" label-width="40%">
+													<el-select class="selectWidth" v-model="templateValue" size="small">
+														<el-option
+															v-for="item in templateList"
+															:key="item.id"
+															:label="item.name"
+															:value="item.name">
+														</el-option>
+													</el-select>
+												</el-form-item>
+											</el-col>
+										</el-row>
+										<el-row>
+											<el-col :span="7">
+												<el-form-item label="动作标识" prop="caseProperty" label-width="40%">
+													<el-input v-model="addSingleForm.testPoint" size="small"></el-input>
+												</el-form-item>
+											</el-col>
+											<el-col :span="7" :offset="1">
+												<el-form-item label="步骤顺序号" prop="caseType" label-width="40%">
+													<el-input v-model="addSingleForm.testPoint" size="small"></el-input>
+													</el-select>
+												</el-form-item>
+											</el-col>
+											<el-col :span="7" :offset="1">
+												<el-form-item label="测试点" prop="priority" label-width="40%">
+													<el-input v-model="addSingleForm.testPoint" size="small"></el-input>
+												</el-form-item>
+											</el-col>
+										</el-row>
+										<el-row>
+											<el-col :span="7" >
+												<el-form-item label="执行方式" prop="executeMethod" label-width="40%">
+													<el-select class="selectWidth" v-model="executeMethod" size="small">
+														<el-option
+															v-for="item in executeMethodList"
+															:key="item.id"
+															:label="item.label"
+															:value="item.value">
+														</el-option>
+													</el-select>
+												</el-form-item>
+											</el-col>
+											<el-col :span="7" :offset="1">
+												<el-form-item label="脚本管理方式" prop="scriptMode" label-width="40%">
+													<el-select class="selectWidth" v-model="scriptMode" size="small">
+														<el-option
+															v-for="item in scriptModeList"
+															:key="item.id"
+															:label="item.label"
+															:value="item.value">
+														</el-option>
+													</el-select>
+												</el-form-item>
+											</el-col>
+										</el-row>
+										<el-row>
+											<el-col :span="11">
+												<el-form-item label="测试意图" prop="testPoint" label-width="25%" required>
+													<el-input v-model="addMultipleForm.testPoint"></el-input>
+												</el-form-item>
+											</el-col>
+											<el-col :span="11" :offset="1">
+												<el-form-item label="前置条件" prop="missionName" label-width="25%"
+															  required>
+													<el-input v-model="addMultipleForm.missionName"></el-input>
+												</el-form-item>
+											</el-col>
+										</el-row>
+										<el-row>
+											<el-col :span="11">
+												<el-form-item label="测试步骤" prop="testPoint" label-width="25%" required>
+													<el-input v-model="addMultipleForm.testPoint"></el-input>
+												</el-form-item>
+											</el-col>
+											<el-col :span="11" :offset="1">
+												<el-form-item label="数据需求" prop="missionName" label-width="25%">
+													<el-input v-model="addMultipleForm.missionName"></el-input>
+												</el-form-item>
+											</el-col>
+										</el-row>
+										<el-row>
+											<el-col :span="11">
+												<el-form-item label="预期结果" prop="testPoint" label-width="25%" required>
+													<el-input v-model="addMultipleForm.testPoint"></el-input>
+												</el-form-item>
+											</el-col>
+											<el-col :span="11" :offset="1">
+												<el-form-item label="附加检查点" prop="missionName" label-width="25%"
+															  required>
+													<el-input v-model="addMultipleForm.missionName"></el-input>
+												</el-form-item>
+											</el-col>
+										</el-row>
+										<el-row>
+											<el-col :span="24">
+												<el-form-item label="备注" prop="" label-width="11.5%">
+													<el-col :span="23">
+														<el-input
+															type="textarea"
+															placeholder="请输入内容"
+															v-model="textarea">
+														</el-input>
+													</el-col>
+												</el-form-item>
+											</el-col>
+
+										</el-row>
+										<el-row>
+											<el-col :span="6" :offset="18">
+												<el-button type="primary" size="small">添加</el-button>
+												<el-button type="primary" size="small">取消</el-button>
+											</el-col>
+										</el-row>
+									</div>
+								</el-form>
+							</div>
+					</el-tabs>
+				</el-dialog>
+				<el-dialog
 					title="导入"
 					:visible.sync="dialogVisibleI"
 					width="30%">
 					<!-- ElementUI上传 -->
 					<el-upload
 						class="upload-demo in-file"
-						action="http://10.101.167.184:8080/atfcloud2.0a/testcase/batchImportTestcase"
+						action=""
+						:on-change="changeFile"
 						:on-preview="handlePreview"
 						:on-remove="handleRemove"
 						:before-remove="beforeRemove"
 						multiple
-						:on-exceed="handleExceed"
 						:file-list="fileList">
 						<el-button size="small" type="primary">选择文件</el-button>
 						<i slot="tip" class="el-upload__tip text">只能选择xlsx文件</i>
 					</el-upload>
-					<el-divider ></el-divider>
+					<el-divider></el-divider>
 					<div>
 						上传方式：
 						<el-radio v-model="radio" label="1">简单版</el-radio>
@@ -744,11 +1010,25 @@
 						<el-radio v-model="radio" label="2" class="radio">标准版</el-radio>
 						<el-button size="small" type="success" @click="downloadTemplate(1)">模板下载</el-button>
 					</div>
-					<el-divider ></el-divider>
+					<el-divider></el-divider>
 					<div class="in-file">
-						<el-button size="small" type="primary">导入</el-button>
-						<el-button size="small" type="primary">取消</el-button>
+						<el-button size="small" type="primary" @click="uploadTemp">导入</el-button>
+						<el-button size="small" type="primary" @click="this.dialogVisibleI = false">取消</el-button>
 					</div>
+				</el-dialog>
+				<el-dialog
+					title="导出"
+					:visible.sync="dialogVisibleO"
+					width="30%">
+					<el-form ref="outputForm" prop="outputForm" label-width="80px">
+						<el-form-item label="导出范围" prop="output">
+							<el-input :disabled="true" placeholder="部分"></el-input>
+						</el-form-item>
+						<div class="in-file">
+							<el-button size="small" type="primary" @click="outputTemp">导出</el-button>
+							<el-button size="small" type="primary" @click="this.dialogVisibleO = false">取消</el-button>
+						</div>
+					</el-form>
 				</el-dialog>
 			</el-main>
 			<el-footer>
@@ -879,17 +1159,21 @@
 					label: '模板'
 				}],
 				testCaseList: [],
+				subCaseList: [],
 				missionList: [],
 				autList: [],
 				transList: [],
 				templateList: [],
 				userList: [],
+				multipleSelection: [],
 				addVisible: false,
 				addVisibleM: false,
 				author: '',
 				executor: '',
 				reviewer: '',
 				fileList: [], //文件列表
+				idList: [],
+				failMSG: "操作失败啦",
 				scriptMode: '模板',
 				useStatus: '评审通过',
 				executeMethod: '自动化',
@@ -901,7 +1185,7 @@
 				templateValue: '',
 				missionValue: '',
 				systemValue: '',
-				radio:'1',
+				radio: '1',
 				// apiUrl: '',
 				tt: 0, //总条数
 				pageSize: 8, //页面大小
@@ -914,6 +1198,8 @@
 				activeName: 'first',
 				dialogVisible: false,
 				dialogVisibleI: false,
+				dialogVisibleO: false,
+				dialogVisibleN:false,
 				modelFlag: 0,
 			}
 		},
@@ -1198,29 +1484,106 @@
 			uploadCase() {
 				this.dialogVisibleI = true
 			},
+			changeFile(file) {
+				this.fileList.push(file)
+			},
 			handleRemove(file, fileList) {
-				console.log(file, fileList);
+				var index = fileList.indexOf(file)
+				this.fileList.splice(index, 1)
 			},
 			handlePreview(file) {
 				console.log(file);
 			},
 			beforeRemove(file, fileList) {
-				return this.$confirm(`确定移除 ${ file.name }？`);
+				return this.$confirm(`确定移除 ${file.name}？`);
+			},
+			//实际导入函数
+			uploadTemp() {
+				let file = this.fileList[0]
+				let param = new FormData()
+				var caseLibId = sessionStorage.getItem("caselibId")
+				param.append("file", file.raw)
+				param.append("caseLibId", caseLibId)
+				param.append('uploadUserId', 3)
+				console.log(param.get("file"))
+				var _this = this
+				Request({
+					url: '/testcase/batchImportTestcase',
+					method: 'post',
+					params: param
+				}).then((res) => {
+					if (res.respCode == "0000") {
+						this.dialogVisibleI = false
+						_this.getCase()
+						this.$alert('导入成功', '导入情况', {
+							confirmButtonText: '确定',
+							callback: action => {
+								this.$message({
+									type: 'info',
+									message: `action: ${ action }`
+								});
+							}
+						});
+					} else {
+						_this.failMSG = res.respMsg
+						this.$alert(_this.failMSG, '导入情况', {
+							confirmButtonText: '确定',
+							callback: action => {
+								this.$message({
+									type: 'info',
+									message: `action: ${ action }`
+								});
+							}
+						});
+					}
+				}).catch((err) => {
+					console.log(err)
+				})
+			},
+			outputCase() {
+				this.dialogVisibleO = true
+			},
+			addNodeShow(){
+				this.dialogVisibleN = true
 			},
 			//下载模板
-			downloadTemplate(val){
-				if(val==0){
-					let url ="http://10.101.167.184:8080/atfcloud2.0a/testcase/batchImport/file/template/simple";
+			downloadTemplate(val) {
+				if (val == 0) {
+					let url = "http://10.101.167.184:8080/atfcloud2.0a/testcase/batchImport/file/template/simple";
 					window.location.href = url;
-				}
-				else{
-					let url ="http://10.101.167.184:8080/atfcloud2.0a/testcase/getStandardExcelTemporary";
+				} else {
+					let url = "http://10.101.167.184:8080/atfcloud2.0a/testcase/getStandardExcelTemporary";
 					window.location.href = url;
 				}
 			},
-			subShow(caseId){
-
+			handleSelectionChange(val) {
+				this.multipleSelection = val
+				for (var i = 0; i < this.multipleSelection; i++) {
+					this.idList.push(this.multipleSelection[i].id)
+				}
 			},
+			subShow(row, rowList) {
+				if (row.caseCompositeType != 1) {
+					var _this = this
+					Request({
+						url: '/testcase/queryTestcaseActionList',
+						method: 'post',
+						params: {'id': row.id}
+					}).then((res) => {
+						_this.subCaseList = res.testcaseActionViewList
+						console.log(_this.subCaseList)
+					}).catch((err) => {
+						console.log(err)
+					})
+				}
+			},
+			// 添加行的索引
+			tableRowClassName({row, rowIndex}) {
+				row.index = rowIndex
+			},
+			toUploadRecord(){
+				this.$router.push({path:'uploadRecord'})
+			}
 		}
 	}
 </script>
@@ -1248,11 +1611,13 @@
 	.in-file {
 		text-align: center;
 	}
-	.text{
-		padding-right:10px
+
+	.text {
+		padding-right: 10px
 	}
-	.radio{
-		padding-left:75px;
+
+	.radio {
+		padding-left: 75px;
 		padding-top: 15px;
 	}
 </style>
