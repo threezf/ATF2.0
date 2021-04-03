910ca1d68d3c54a2705ee86693f7e2d35c372ada
付寿东 <2407592711@qq.com>
2021-04-03 08:40:34 +0800

    添加说明

---
+++ src/components/help/anchor.vue
@@ -0,0 +1,175 @@
+<template>
+    <div class="slider-main">
+        <div class="slider-body">
+            <div ref="sliderMenu" class="slider-menu">
+                <div class="slider-menu-item" v-for="(item, index) in originData" :key='index' :style="item.style || {}"
+                :class="{'slider-menu-item-active': current === index}" @click="scrollToView(index)">
+                <span>{{item.name}}</span>
+                </div>
+            </div>
+            <div class="slider-info">
+                <div ref="scrollItem" class="component-info" v-for="(item, index) in originData" :key="index + '-cmp'">
+                    <component :is="item.component"></component>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestProject',
+    props: {
+        originData: {
+            type: [String, Number, Boolean, Object, Array],
+            default: () => [
+                {name: '技术能力', style: {}, component: {}},
+                {name: '语音技术', style: {}, component: {}},
+                {name: '文字识别', style: {}, component: {}},
+                {name: '人脸与人体识别', style: {}, component: {}},
+                {name: '视频技术', style: {}, component: {}},
+                {name: 'AR与VR', style: {}, component: {}},
+                {name: '数据智能', style: {}, component: {}}
+            ]
+        }
+    },
+    data () {
+        return {
+           current: 0
+        }
+    },
+    methods: {
+        scrollToView (index) {
+            let data = this.getScrollItems()
+            if (index >= 0 && index < data.length) {
+                let item = data[index] || {}
+                const scrollTop = this.getScroll(window, true)
+                const elOffset = this.getOffset(item)
+                let top = elOffset.top - this.getStartTop()
+                window.scrollTo({top: top < 0 ? 0 : top, behavior: 'smooth'})
+            }
+        },
+        handleScroll (e) {
+            let menu = document.querySelector('.slider-menu')
+            console.log(e.path[1].scrollY, menu)
+            if(e.path[1].scrollY > 120) {
+                menu.style.top = '5px'
+            }else {
+                menu.style.top = 125 - e.path[1].scrollY + 'px'
+            }
+            let data = this.getScrollItems()
+            const scrollTop = this.getScroll(window, true)
+            let maxTop = -99999
+            data.forEach((target, index) => {
+                const elOffset = this.getOffset(target)
+                let realTop = elOffset.top - this.getStartTop() - scrollTop
+                if (realTop <= 0 && maxTop <= realTop) {
+                    maxTop = realTop
+                    this.current = index
+                }
+            })
+        },
+        getStartTop () {
+            if (this.$refs.sliderMenu) {
+                return this.$refs.sliderMenu.offsetTop || 0
+            }
+            return 0
+        },
+        getScrollItems () {
+            let refs = []
+            if (this.$refs.scrollItem && this.$refs.scrollItem.style) {
+                refs.push(this.$refs.scrollItem)
+            } else if (this.$refs.scrollItem && this.$refs.scrollItem.length) {
+                refs = this.$refs.scrollItem
+            }
+            return refs
+        },
+        getScroll (target, top) {
+            const prop = top ? 'pageYOffset' : 'pageXOffset'
+            const method = top ? 'scrollTop' : 'scrollLeft'
+            let ret = target[prop]
+            if (typeof ret !== 'number') {
+                ret = window.document.documentElement[method]
+            }
+            return ret
+        },
+        getOffset (element) {
+            const rect = element.getBoundingClientRect()
+            const scrollTop = this.getScroll(window, true)
+            const scrollLeft = this.getScroll(window)
+        
+            const docEl = window.document.body
+            const clientTop = docEl.clientTop || 0
+            const clientLeft = docEl.clientLeft || 0
+            let height = rect.bottom - rect.top
+            if (height === 0 && element.parentNode) {
+                let parentRect = element.parentNode.getBoundingClientRect()
+                height = parentRect ? parentRect.height || 0 : 0
+            }
+            return {
+                top: rect.top + scrollTop - clientTop,
+                left: rect.left + scrollLeft - clientLeft,
+                height: height,
+                width: rect.right - rect.left
+            }
+        }
+    },
+    mounted () {
+        window.addEventListener('scroll', this.handleScroll, false)
+        window.addEventListener('resize', this.handleScroll, false)
+    },
+    beforeDestroy () {
+        window.removeEventListener('scroll', this.handleScroll, false)
+        window.removeEventListener('resize', this.handleScroll, false)
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+    .slider-main {
+        display: flex;
+        flex-direction: column;
+    }
+    .slider-body {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .slider-menu {
+        position: fixed;
+        left: auto;
+        margin-left: -15px;
+        margin-right: -10px;
+        width: fit-content;
+        overflow: auto;
+        color: #000;
+        height: auto;
+        top: 120px;
+    }
+    .slider-menu-item {
+        border-left: 2px solid #e8eaed;
+        display: block;
+        padding: 15px 0 0 30px;
+        position: relative;
+        line-height: 24px;
+        color: inherit;
+        cursor: pointer;
+    }
+    .slider-menu-item-active, .slider-menu-item:hover {
+        color: #1a73e8;
+    }
+    .slider-menu-item-active {
+        border-left: 2px solid #1a73e8;
+    }
+    .component-info {
+        // height: 700px;
+        // width: 600px;
+        border: 1px solid #f0f0f0;
+    }
+    .slider-info {
+        margin-left: 185px;
+        display: flex;
+        flex-direction: column;
+        position: relative;
+    }
+</style>
--- src/views/help/testProject/index.vue
+++ src/views/help/testProject/index.vue
@@ -1,19 +1,32 @@
 <template>
     <div class="project">
-        <mavon-editor class="markdown"
-          :value='msg'
-          :subfield = "false"    
-          :defaultOpen = "prop.defaultOpen"
-          :toolbarsFlag = "prop.toolbarsFlag"
-          :editable="prop.editable"
-          :scrollStyle="prop.scrollStyle"
-      ></mavon-editor>
+        <el-card>
+            <div class="component">
+                <!-- <ManageProject></ManageProject>
+                <ManageCase></ManageCase>
+                <ManageSource></ManageSource>
+                <TestPlan></TestPlan> -->
+                <Anchor :originData="originData" class="anchor"></Anchor>
+            </div>
+        </el-card>
     </div>
 </template>
 
 <script>
+import Anchor from '@/components/help/anchor.vue'
+import ManageProject from './manageProject.vue'
+import ManageCase from './manageCase.vue'
+import ManageSource from './manageSource.vue'
+import TestPlan from './testPlan.vue'
 export default {
     name: 'TestProject',
+    components: {
+        ManageProject,
+        ManageCase,
+        ManageSource,
+        TestPlan,
+        Anchor
+    },
     props: {
         url: {
             type: String,
@@ -22,7 +35,25 @@ export default {
     },
     data () {
         return {
-           msg: ''
+           msg: '',
+           originData: [{
+               name: '测试项目的建立与管理', 
+               style: {}, 
+               component: ManageProject
+            }, {
+                name: '测试用例管理', 
+                style: {}, 
+                component: ManageCase
+            }, {
+                name: '测试资源管理', 
+                style: {}, 
+                component: ManageSource
+            }, {
+                name: '测试计划与执行', 
+                style: {}, 
+                component: TestPlan
+            }]
+           
         }
     },
     computed: {
@@ -46,16 +77,28 @@ export default {
         }
     },
     mounted() {
-        this.readFile()
+        // this.readFile()
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
     .project {
         padding: 15px;
         height: 100%;
         overflow-y: scroll;
     }
+    .main-title {
+        padding-bottom: .3em;
+        font-size: 2em;
+        border-bottom: 1px solid #eaecef;
+    }
+    .anchor {
+        height: calc(100% - 89px);
+    }
+    .component {
+    }
 </style>
+
+
